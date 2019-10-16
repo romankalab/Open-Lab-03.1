28151edf7ceb3cdee198080bb8b225e3baa6b872
romankalab <roan.kalab@mail.com>
2019-10-16 12:29:07 +0200

    Initial

---
+++ Open-Lab-03.1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Open_Lab_03._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
